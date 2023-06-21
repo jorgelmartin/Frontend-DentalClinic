a04d3410711ae9c0983f18772e6e1c08634bb5f3
Jorge Martin <jorgemctin@gmail.com>
2023-06-22 01:05:29 +0200

    Fix: Header component

--- src/services/ApiCalls.js
+++ src/services/ApiCalls.js
@@ -3,15 +3,19 @@ import axios from 'axios';
 
 const root = "https://makeup-api.herokuapp.com/api/v1"
 
-export const bringProducts = async () => {
+// export const bringProducts = async () => {
 
-    return await axios.get(`https://rickandmortyapi.com/api/character/?page=17`);
-};
+//     return await axios.get(`https://rickandmortyapi.com/api/character/?page=17`);
+// };
 
 export const loginMe = async (credentials) => {
 
     //Esto es un ejemplo de como enviamos un body por axios en un protocolo POST
-    return await axios.post(`urldelbackendqueseencargadellogin`, credentials);
+    return await axios.post(`http://localhost:4000/auth/login`, credentials);
 
 
 }
+
+// export const registerMe = async (credentials) => {
+//     return await axios.post(`http://localhost:9000/auth/register`, credentials)
+// }
