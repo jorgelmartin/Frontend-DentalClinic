2edcc43fba1cfb29c3bbdd3e9ccdbb6db68cb3ec
Jorge Martin <jorgemctin@gmail.com>
2023-06-24 15:11:43 +0200

    Feat: About page

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -4,16 +4,20 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "../Home/Home";
 import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
+// import { About } from "../About/About";
+import { Admin } from "../Admin/Admin";
 
 export const Body = () => {
     return (
         <>
             <Routes>
                 <Route path="*" element={<Navigate to="/"/>} />
-                <Route path="*" element={<Home />} />
-                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Admin />} />
+                <Route path="/" element={<Admin />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                {/* <Route path="/about" element={<About />} /> */}
+                <Route path="/admin" element={<Admin />} />
             </Routes>
         </>
     );
