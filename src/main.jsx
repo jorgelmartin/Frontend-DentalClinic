812c3cce2ce8300d3eb7154b074d10a2dc9d5a45
Jorge Martin <jorgemctin@gmail.com>
2023-06-24 15:12:06 +0200

    Feat: redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
+//REDUX
+import { Provider } from "react-redux";
+import store from "./app/store";
+
+//REDUX PERSISTENCE....
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
 import { BrowserRouter } from "react-router-dom";
 
+const persistor = persistStore(store);
+
+//ENVUELVE TODO PARA PONER INTERACTUAR
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
