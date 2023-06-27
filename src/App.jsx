898328c78f5193fb7d5c2d63e18212a4226f40be
Jorge Martin <jorgemctin@gmail.com>
2023-06-27 18:45:34 +0200

    Refactor: Register Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import './App.css'
 import { Header } from './common/Header/Header'
 // import { ChangeView } from './common/ChangeView/ChangeView'
 import { Body } from './pages/Body/Body'
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,101 +1,197 @@
-import React from "react";
+import React, { useState } from "react"; 1
 import "./Register.css";
 import { InputText } from "../../common/InputText/InputText";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
-
-
+import { checkError } from "../../services/usefull";
+import { useNavigate } from "react-router-dom";
+import { registerMe } from "../../services/ApiCalls";
 
 export const Register = () => {
-    const inputFields = [
-        { label: 'Nombre completo:', type: 'text', placeholder: 'Escribe tu nombre...', name: 'fullname' },
-        { label: 'Email:', type: 'email', placeholder: 'Escribe tu email...', name: 'email' },
-        // Agrega el resto de los inputs aquí
-    ];
+
+    const [credentials, setCredentials] = useState({
+        fullname: "",
+        email: "",
+        password: "",
+        dni: "",
+        address: "",
+        age: "",
+        phone: "",
+    });
+
+    const [credentialsError, setCredentialsError] = useState({
+        fullname: "",
+        email: "",
+        password: "",
+        dni: "",
+        address: "",
+        age: "",
+        phone: "",
+    });
+
+    const inputHandler = (e) => {
+
+        setCredentials((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const inputCheck = (e) => {
+
+        let mensajeError = checkError(e.target.name, e.target.value);
+
+        setCredentialsError((prevState) => ({
+            ...prevState,
+            [e.target.name + "Error"]: mensajeError,
+        }));
+    };
+
+    const regMe = () => {
+
+        registerMe(credentials)
+            .then(() => {
+
+                setTimeout(() => {
+                    navigate("/profile");
+                }, 2000);
+
+            })
+            .catch((error) => console.log(error));
+    };
 
     return (
         <div className="registerDesign">
             <Container className="d-flex justify-content-center align-items-center mt-4">
                 <Card>
-                    
+
                     <Card.Body>
                         <Card.Title className="text-center mb-3 display-5">Registro</Card.Title>
                         <Form>
                             <Form.Group as={Row} className="mb-2">
                                 <Form.Label column>Nombre completo:</Form.Label>
                                 <Col>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Escribe tu nombre..."
-                                        name="fullname"
+                                    <InputText
+                                        type={"text"}
+                                        design={
+                                            credentialsError.fullnameError === ""
+                                                ? "normalInputRegister"
+                                                : "normalInputRegister errorInput"
+                                        }
+                                        placeholder={"Ingrese su nombre..."}
+                                        name={"fullname"}
+                                        functionHandler={inputHandler}
+                                        onBlurFunction={inputCheck}
                                     />
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mb-2">
                                 <Form.Label column>Email:</Form.Label>
                                 <Col>
-                                    <Form.Control
-                                        type="email"
-                                        placeholder="Escribe tu email..."
-                                        name="email"
+                                    <InputText
+                                        type={"email"}
+                                        design={
+                                            credentialsError.emailError === ""
+                                                ? "normalInputRegister"
+                                                : "normalInputRegister errorInput"
+                                        }
+                                        placeholder={"Ingrese su email..."}
+                                        name={"email"}
+                                        functionHandler={inputHandler}
+                                        onBlurFunction={inputCheck}
                                     />
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mb-2">
                                 <Form.Label column>Contraseña:</Form.Label>
                                 <Col>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder="Escribe tu contraseña..."
-                                        name="password"
+                                    <InputText
+                                        type={"password"}
+                                        design={
+                                            credentialsError.passwordError === ""
+                                                ? "normalInputRegister"
+                                                : "normalInputRegister errorInput"
+                                        }
+                                        placeholder={"Ingrese su contraseña..."}
+                                        name={"password"}
+                                        functionHandler={inputHandler}
+                                        onBlurFunction={inputCheck}
                                     />
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mb-2">
                                 <Form.Label column>DNI/NIE:</Form.Label>
                                 <Col>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Escribe tu DNI..."
-                                        name="nif"
+                                    <InputText
+                                        type={"text"}
+                                        design={
+                                            credentialsError.addressError === ""
+                                                    ? "normalInputRegister"
+                                                : "normalInputRegister errorInput"
+                                        }
+                                        placeholder={"Ingrese su dirección..."}
+                                        name={"address"}
+                                        functionHandler={inputHandler}
+                                        onBlurFunction={inputCheck}
                                     />
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mb-2">
                                 <Form.Label column>Dirección:</Form.Label>
                                 <Col>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Escribe tu dirección..."
-                                        name="address"
+                                    <InputText
+                                        type={"text"}
+                                        design={
+                                            credentialsError.dniError === ""
+                                                ? "normalInputRegister"
+                                                : "normalInputRegister errorInput"
+                                        }
+                                        placeholder={"Ingrese su dni..."}
+                                        name={"dni"}
+                                        functionHandler={inputHandler}
+                                        onBlurFunction={inputCheck}
                                     />
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mb-2">
                                 <Form.Label column>Edad:</Form.Label>
                                 <Col>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Escribe tu edad..."
-                                        name="age"
+                                    <InputText
+                                        type={"text"}
+                                        design={
+                                            credentialsError.ageError === ""
+                                                ? "normalInputRegister"
+                                                : "normalInputRegister errorInput"
+                                        }
+                                        placeholder={"Ingrese su edad..."}
+                                        name={"age"}
+                                        functionHandler={inputHandler}
+                                        onBlurFunction={inputCheck}
                                     />
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mb-2">
                                 <Form.Label column>Teléfono:</Form.Label>
                                 <Col>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Escribe tu teléfono..."
-                                        name="phone"
+                                    <InputText
+                                        type={"text"}
+                                        design={
+                                            credentialsError.phoneError === ""
+                                                ? "normalInputRegister"
+                                                : "normalInputRegister errorInput"
+                                        }
+                                        placeholder={"Ingrese su teléfono..."}
+                                        name={"phone"}
+                                        functionHandler={inputHandler}
+                                        onBlurFunction={inputCheck}
                                     />
                                 </Col>
                             </Form.Group>
                             <div className="text-center">
-                <Button style={{ backgroundColor: '#13326fba' }} className="w-50" type="submit">
-                  Registro
-                </Button>
-              </div>
+                                <Button onClick={() => regMe()} style={{ backgroundColor: '#13326fba' }} className="w-50" type="submit">
+                                    Registro
+                                </Button>
+                                
+                            </div>
                         </Form>
                     </Card.Body>
                 </Card>
@@ -103,7 +199,6 @@ export const Register = () => {
         </div>
     );
 };
-
 
 
 // import React from 'react';
