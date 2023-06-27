fab42cd1a63ef78b29dd935462285367fd896225
Jorge Martin <jorgemctin@gmail.com>
2023-06-27 18:46:28 +0200

    Refactor: Profile Card

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./Profile.css";
+import { AppointmentCard } from "../../common/AppointmentCard/AppointmentCard";
+import { ProfileCard } from "../../common/ProfileCard/ProfileCard";
+import { userData } from "../userSlice";
+
+
 
 export const Profile = () => {
 
@@ -16,6 +21,9 @@ export const Profile = () => {
                                 <Button style={{ backgroundColor: '#13326fba' }} className="w-100">PERFIL</Button>
                             </Card.Body>
                         </Card>
+                        <div className="appointments">
+                            <ProfileCard></ProfileCard>
+                        </div>
                     </Col>
                     <Col xs={7} md={6} lg={3}>
                         <Card style={{ backgroundColor: '#33468d58' }} border="dark" text="dark" className="adminCardTitle">
@@ -24,15 +32,20 @@ export const Profile = () => {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col xs={7} md={6} lg={3}>
+                    <Col xs={7} md={6} lg={3} className="">
                         <Card style={{ backgroundColor: '#33468d58' }} border="dark" text="dark" className="adminCardTitle">
                             <Card.Body>
                                 <Button style={{ backgroundColor: '#13326fba' }} className="w-100">MIS CITAS</Button>
                             </Card.Body>
                         </Card>
+                        <div className="appointments">
+                            <AppointmentCard></AppointmentCard>
+                        </div>
                     </Col>
+
                 </Row>
             </Container>
+
         </div>
     )
 }
