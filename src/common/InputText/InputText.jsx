15206b88e548e529c5a1cf0466414d49710bbeb5
Jorge Martin <jorgemctin@gmail.com>
2023-06-27 18:46:52 +0200

    Refactor: InputText color

--- src/common/InputText/InputText.jsx
+++ src/common/InputText/InputText.jsx
@@ -1,18 +1,18 @@
 
 import React from 'react';
 import './InputText.css';
- 
-export const InputText = ({type, design, placeholder, name, functionHandler, onBlurFunction}) => {
-     return (
-         <>
-            <input 
-                type={type} 
+
+export const InputText = ({ type, design, placeholder, name, functionHandler, onBlurFunction }) => {
+    return (
+        <>
+            <input
+                type={type}
                 className={design}
                 placeholder={placeholder}
                 name={name}
-                onChange={(e)=>functionHandler(e)}
-                onBlur={(e)=>onBlurFunction(e)}
+                onChange={(e) => functionHandler(e)}
+                onBlur={(e) => onBlurFunction(e)}
             />
-         </>
-     )
+        </>
+    )
 }
