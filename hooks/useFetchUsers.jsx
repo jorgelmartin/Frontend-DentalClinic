63be56bffce73152a35722119c122d9414e2744c
Jorge Martin <jorgemctin@gmail.com>
2023-07-02 00:33:19 +0200

    Feat: UserCard and GetAllUsers endpoint

---
+++ hooks/useFetchUsers.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+export const useFetchUsers = () => {
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/user/getAllUsers')
+            .then(res => res.json())
+            .then(res => {
+                console.log("Response from API:", res);
+                setUsers(res.data);
+            })
+            .catch(error => console.log("Error fetching users:", error))
+    }, []);
+
+    console.log("Users:", users);
+
+    return users;
+};
---
+++ src/common/UserCard/UserCard.jsx
@@ -0,0 +1,67 @@
+
+import React from "react";
+import { useFetchUsers } from "../../../hooks/useFetchUsers";
+import { Col, Row, Container, Form, Card } from "react-bootstrap";
+import "./UserCard.css"
+// import { getAllUsers } from "../../services/ApiCalls";
+
+export const UserCard = () => {
+
+    const users = useFetchUsers();
+    if (!users) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        
+
+<Container fluid className="mt-2">
+  <div className="userCardsRow">
+    {users.map((user) => (
+      <Card key={user.id} className="ucard">
+        <Card.Body>
+          <Form>
+            <Form.Group as={Row}>
+              <Form.Label column xs={5} sm={5}>Nombre:</Form.Label>
+              <Col xs={7} sm={7}>
+                <div>{user.name}</div>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column xs={5} sm={5}>Apellidos:</Form.Label>
+              <Col xs={7} sm={7}>
+                <div>{user.lastname}</div>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column xs={5} sm={5}>Email:</Form.Label>
+              <Col xs={7} sm={7}>
+                <div>{user.email}</div>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column xs={5} sm={5}>DNI/NIE:</Form.Label>
+              <Col xs={7} sm={7}>
+                <div>{user.dni}</div>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column xs={5} sm={5}>Dirección:</Form.Label>
+              <Col xs={7} sm={7}>
+                <div>{user.address}</div>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column xs={5} sm={5}>Teléfono:</Form.Label>
+              <Col xs={7} sm={7}>
+                <div>{user.phone}</div>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Card.Body>
+      </Card>
+    ))}
+  </div>
+</Container>
+    );
+}
