af24a774dbe5cf1beec565ea495303ea121a6861
Jorge Martin <jorgemctin@gmail.com>
2023-09-09 18:57:08 +0200

    Refactor: all Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { ProductCard } from "../../common/ProductCard/ProductCard";
 import "./Home.css";
+import { Container } from "react-bootstrap";
 
 //SHOW SERVICES AT HOME PAGE
 export const Home = () => {
     return (
-        <div className="homeDesign">
-            <ProductCard></ProductCard>
-        </div>
+        <Container className="mt-5">
+            <div className="clinicTitle mt-3"
+                style={{
+                    textShadow: '0.05em 0.05em 0.06em rgba(0, 0, 0, 0.5)'
+                }}>SMILE - everyday
+            </div>
+            <ProductCard />
+        </Container>
     );
 };   
