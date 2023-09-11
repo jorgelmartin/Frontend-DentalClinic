a5f5f5fb6a7f10d75b60ee4208a43c46a3021d56
Jorge Martin <jorgemctin@gmail.com>
2023-09-11 13:01:20 +0200

    Refactor: Profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./Profile.css";
 import { ProfileCard } from "../../common/ProfileCard/ProfileCard";
 
 export const Profile = () => {
     return (
-        <div className="userContainer">
-            <Container className="container-max-width">
-                <Row className="userCard gap-3">
-                    <Col>
-                        <div className="userCard">
+            <Container className="containerProfile">    
                             <ProfileCard></ProfileCard>
-                        </div>
-                    </Col>
-                </Row>
             </Container>
-        </div>
     )
 }
