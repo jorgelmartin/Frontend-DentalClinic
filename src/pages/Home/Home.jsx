b4ae73845d8801c72e8234c5ef9a4ee265ca7126
Jorge Martin <jorgemctin@gmail.com>
2024-07-23 15:59:28 +0200

    Feat: loader home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { Container } from "react-bootstrap";
 import { useFetchServices } from '../../../hooks/useFetchServices';
@@ -8,28 +8,60 @@ import { useNavigate } from "react-router-dom";
 export const Home = () => {
     const services = useFetchServices();
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+
+    // SHOW LOADER 1 TIME & SAVE IN SESSIONSTORAGE
+    useEffect(() => {
+        const hasShownLoader = sessionStorage.getItem('hasShownLoader');
+        if (!hasShownLoader) {
+            const timer = setTimeout(() => {
+                setLoading(false);
+                sessionStorage.setItem('hasShownLoader', 'true');
+            }, 3000);
+
+            return () => clearTimeout(timer);
+        } else {
+            setLoading(false);
+        }
+    }, []);
 
     if (!services) {
         return <div>Cargando...</div>;
     };
 
     return (
-        <Container className="mt-5">
-            <div className="clinicTitle">SMILE - everyday
-            </div>
-            {/* MAPPING SERVICES AT HOME PAGE */}
-            <div className="productCardContainer mt-2">
-                {services.map((service) => (
+        <Container>
 
-                    <div key={service.id} className="productCardDesign">
-                        <img className="productImgDesign"
-                            src={`https://backend-dental-clinic.vercel.app/${service.image}`}
-                            alt={service.name}
-                            onClick={() => navigate(`/servicedetail/${service.id}`)} />
-                        <h1>{service.name}</h1>
+            {/* LOADER */}
+            {loading ? (
+                <div>
+                    <div className="loader">
+                        <div className="intern"></div>
+                        <div className="externalShadow">
+                            <div className="central"></div>
+                        </div>
                     </div>
-                ))}
-            </div>
+                </div>
+            ) : (
+                <div>
+                    {/* TITLE */}
+                    <div className="clinicTitle">SMILE - everyday</div>
+                    {/* MAPPING SERVICES AT HOME PAGE */}
+                    <div className="productCardContainer mt-2">
+                        {services.map((service) => (
+                            <div key={service.id} className="productCardDesign">
+                                <img
+                                    className="productImgDesign"
+                                    src={`https://backend-dental-clinic.vercel.app/${service.image}`}
+                                    alt={service.name}
+                                    onClick={() => navigate(`/servicedetail/${service.id}`)}
+                                />
+                                <h1>{service.name}</h1>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </Container>
     );
 };   
